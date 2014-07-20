35723f3ad3ebc3c0c4bf17b35b7fca11482ce211
Scott Anderson <ascent12@hotmail.com>
2014-07-20 20:48:15 +1200

    Added small torrent test

--- Makefile
+++ Makefile
@@ -1,5 +1,17 @@
+CC = gcc
+CXX = g++
+
+CFLAGS = -c
+
+LDFLAGS = $(shell pkg-config --libs libtorrent-rasterbar)
+
 all:
 	$(MAKE) -C core
 
+test: all
+	$(CC) $(CFLAGS) test.c -o test.o
+	$(CXX) test.o gtorrent-c.a -o test $(LDFLAGS)
+
 clean:
 	$(MAKE) -C core clean
+	rm test test.o
---
+++ test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "core/gtorrent-core.h"
+
+#ifdef WIN32
+#include <windows.h>
+#define sleep(t) Sleep(t*1000)
+#else
+#include <unistd.h>
+#endif
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		printf("USAGE: %s path_to_torrent", argv[0]);
+			return 1;
+	}
+
+	gtorrent_core *g = core_create();
+	printf("Core created! Current location is: %p\n", g);
+
+	gtorrent_torrent *t = add_torrent(g, argv[1]);
+
+	if (!t) {
+		printf("Invalid Torrent!\n");
+		return 1;
+	}
+
+	printf("Downloading %s...\n", get_path(t));
+	while (1) {
+		printf("%s DOWN:%d UP:%d SEEDS:%d PEERS:%d [%d/%d]\n",
+				get_text_state(t),
+				get_download_rate(t),
+				get_upload_rate(t),
+				get_total_seeders(t),
+				get_total_peers(t),
+				get_size(t),
+				get_total_progress(t)
+				);
+		sleep(1);
+	}
+}
