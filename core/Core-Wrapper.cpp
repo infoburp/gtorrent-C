8738cfbabc93cdbfcabcfd932bfe29c6a8aa619f
Scott Anderson <ascent12@hotmail.com>
2014-07-22 09:30:09 +1200

    Update Torrent to gt namespace

--- core/Core-Wrapper.cpp
+++ core/Core-Wrapper.cpp
@@ -27,7 +27,7 @@ const char* get_default_save_path(gt_core *g)
 
 gt_torrent** get_torrents(gt_core *g)
 {
-	vector<shared_ptr<Torrent>> vec = reinterpret_cast<gt::Core*>(g)->getTorrents();
+	vector<shared_ptr<gt::Torrent>> vec = reinterpret_cast<gt::Core*>(g)->getTorrents();
 
 	gt_torrent **array = (gt_torrent**)malloc(sizeof(gt_torrent*)*vec.size());
 
@@ -40,7 +40,7 @@ gt_torrent** get_torrents(gt_core *g)
 gt_torrent* add_torrent(gt_core *g, char *path)
 {
 	string str(path);
-	Torrent *torrent = reinterpret_cast<gt::Core*>(g)->addTorrent(str).get();
+	gt::Torrent *torrent = reinterpret_cast<gt::Core*>(g)->addTorrent(str).get();
 	return reinterpret_cast<gt_torrent*>(torrent);
 }
 
@@ -61,7 +61,7 @@ void update(gt_core *g)
 
 int32_t get_number_torrents(gt_core *g)
 {
-	vector<shared_ptr<Torrent>> vec = reinterpret_cast<gt::Core*>(g)->getTorrents();
+	vector<shared_ptr<gt::Torrent>> vec = reinterpret_cast<gt::Core*>(g)->getTorrents();
 	return vec.size();
 }
 
@@ -70,22 +70,22 @@ int32_t get_number_torrents(gt_core *g)
 bool poll_event(gt_torrent *t, gt_event *e)
 {
 	gt::Event *event = reinterpret_cast<gt::Event*>(e);
-	return reinterpret_cast<Torrent*>(t)->pollEvent(*event);
+	return reinterpret_cast<gt::Torrent*>(t)->pollEvent(*event);
 }
 
 bool is_paused(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->isPaused();
+	return reinterpret_cast<gt::Torrent*>(t)->isPaused();
 }
 
 int64_t get_active_time(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getActiveTime();
+	return reinterpret_cast<gt::Torrent*>(t)->getActiveTime();
 }
 
 int get_state(gt_torrent *t)
 {
-	switch (reinterpret_cast<Torrent*>(t)->getState()) {
+	switch (reinterpret_cast<gt::Torrent*>(t)->getState()) {
 	case libtorrent::torrent_status::checking_files:
 		return TORRENT_STATE_CHECKING_FILES;
 	case libtorrent::torrent_status::seeding:
@@ -97,158 +97,158 @@ int get_state(gt_torrent *t)
 
 float get_total_progress(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getTotalProgress();
+	return reinterpret_cast<gt::Torrent*>(t)->getTotalProgress();
 }
 
 uint32_t get_upload_rate(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getUploadRate();
+	return reinterpret_cast<gt::Torrent*>(t)->getUploadRate();
 }
 
 uint32_t get_download_rate(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getDownloadRate();
+	return reinterpret_cast<gt::Torrent*>(t)->getDownloadRate();
 }
 
 uint32_t get_ppm_progress(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getPPMProgress();
+	return reinterpret_cast<gt::Torrent*>(t)->getPPMProgress();
 }
 
 uint32_t get_total_seeders(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getTotalSeeders();
+	return reinterpret_cast<gt::Torrent*>(t)->getTotalSeeders();
 }
 
 uint32_t get_total_peers(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getTotalPeers();
+	return reinterpret_cast<gt::Torrent*>(t)->getTotalPeers();
 }
 
 uint32_t get_total_leechers(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getTotalLeechers();
+	return reinterpret_cast<gt::Torrent*>(t)->getTotalLeechers();
 }
 
 int64_t get_total_uploaded(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getTotalUploaded();
+	return reinterpret_cast<gt::Torrent*>(t)->getTotalUploaded();
 }
 
 int64_t get_total_downloaded(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getTotalDownloaded();
+	return reinterpret_cast<gt::Torrent*>(t)->getTotalDownloaded();
 }
 
 int64_t get_size(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getSize();
+	return reinterpret_cast<gt::Torrent*>(t)->getSize();
 }
 
 int64_t get_wanted(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getWanted();
+	return reinterpret_cast<gt::Torrent*>(t)->getWanted();
 }
 
 int64_t get_torrent_size(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getTorrentSize();
+	return reinterpret_cast<gt::Torrent*>(t)->getTorrentSize();
 }
 
 int64_t get_time_remaining(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getTimeRemaining();
+	return reinterpret_cast<gt::Torrent*>(t)->getTimeRemaining();
 }
 
 float get_total_ratio(gt_torrent *t)
 {
-	return reinterpret_cast<Torrent*>(t)->getTotalRatio();
+	return reinterpret_cast<gt::Torrent*>(t)->getTotalRatio();
 }
 
 char* get_path(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getPath().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getPath().c_str());
 }
 
 char* get_current_tracker_url(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getCurrentTrackerURL().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getCurrentTrackerURL().c_str());
 }
 
 char* get_text_active_time(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextActiveTime().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextActiveTime().c_str());
 }
 
 char* get_text_state(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextState().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextState().c_str());
 }
 
 char* get_text_upload_rate(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextUploadRate().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextUploadRate().c_str());
 }
 
 char* get_text_download_rate(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextDownloadRate().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextDownloadRate().c_str());
 }
 
 char* get_text_total_uploaded(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextTotalUploaded().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextTotalUploaded().c_str());
 }
 
 char* get_text_total_downloaded(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextTotalDownloaded().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextTotalDownloaded().c_str());
 }
 
 char* get_text_size(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextSize().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextSize().c_str());
 }
 
 char* get_text_remaining(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextRemaining().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextRemaining().c_str());
 }
 
 char* get_text_total_ratio(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextTotalRatio().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextTotalRatio().c_str());
 }
 
 char* get_text_time_remaining(gt_torrent *t)
 {
-	return const_cast<char*>(reinterpret_cast<Torrent*>(t)->getTextTimeRemaining().c_str());
+	return const_cast<char*>(reinterpret_cast<gt::Torrent*>(t)->getTextTimeRemaining().c_str());
 }
 
 void set_save_path(gt_torrent *t, char *path)
 {
 	string savepath(path);
-	reinterpret_cast<Torrent*>(t)->setSavePath(savepath);
+	reinterpret_cast<gt::Torrent*>(t)->setSavePath(savepath);
 }
 
 //void force_recheck(gt_torrent *t)
 //{
-//	reinterpret_cast<Torrent*>(t)->torrentForceRecheck();
+//	reinterpret_cast<gt::Torrent*>(t)->torrentForceRecheck();
 //}
 
 void set_paused(gt_torrent *t, bool paused)
 {
-	reinterpret_cast<Torrent*>(t)->setPaused(paused);
+	reinterpret_cast<gt::Torrent*>(t)->setPaused(paused);
 }
 
 void t_resume(gt_torrent *t)
 {
-	reinterpret_cast<Torrent*>(t)->resume();
+	reinterpret_cast<gt::Torrent*>(t)->resume();
 }
 
 void t_pause(gt_torrent *t)
 {
-	reinterpret_cast<Torrent*>(t)->pause();
+	reinterpret_cast<gt::Torrent*>(t)->pause();
 }
 
 } // extern "C"
